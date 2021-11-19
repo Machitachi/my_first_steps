aac868ec15863b2b2eaf3ebec68e8915678c768e
Paulo Alexandre <pas.cje.empreender@gmail.com>
2021-11-19 07:41:26 -0300

    Hobby - usando página dinamica

---
+++ front_end/aula-24-css-html-js/src/script.js
@@ -0,0 +1,83 @@
+// Title
+let title = document.getElementById("title");
+title.innerHTML = "Paulo Alexandre";
+title.style.fontSize = "30px";
+title.style.color = "red";
+
+let eu = document.getElementById("foto");
+eu.src = "img/eu.jpg";
+eu.style.borderRadius = "50%";
+
+let p1 = document.getElementById("estado");
+p1.innerHTML = "Guarulhos / São Paulo";
+
+
+
+// modalidade 1 - ping pong
+let modalidade1 = document.getElementById("mod-1");
+modalidade1.innerHTML = "Hobby 1 - ping pong";
+modalidade1.style.fontSize = "25px";
+modalidade1.style.color = "red";
+
+// modalidade 2 - Futebol
+let modalidade2 = document.getElementById("mod-2");
+modalidade2.innerHTML = "Hobby 2- Futebol";
+modalidade2.style.fontSize = "25px";
+modalidade2.style.color = "red";
+
+// modalidade 3 - Escalar
+let modalidade3 = document.getElementById("mod-3");
+modalidade3.innerHTML = "Hobby 3 - Escalar";
+modalidade3.style.fontSize = "25px";
+modalidade3.style.color = "red";
+
+// modalidade 4 - Andar de Bicicleta
+let modalidade4 = document.getElementById("mod-4");
+modalidade4.innerHTML = "Hobby 4 - Andar";
+modalidade4.style.fontSize = "25px";
+modalidade4.style.color = "red";
+
+let description1 = document.getElementById("paragraph1");
+description1.innerHTML =  "Tênis de mesa, também conhecido como pingue-pongue, é o jogo em que duas pessoas ou duplas usam raquetes de madeira para passar uma bolinha de um lado a outro de uma rede instalada em uma mesa. O nome pingue-pongue deve-se ao barulho que a bola faz ao bater na raquete e na mesa."
+
+let description2 = document.getElementById("paragraph2");
+description2.innerHTML =  "Futebol é o esporte coletivo mais praticado do mundo. É disputado por duas equipes, de 11 jogadores que têm como objetivo colocar a bola entre as traves adversárias o maior número de vezes sem usar mãos e braços. Esse objetivo é chamado gol ou golo."
+
+let description3 = document.getElementById("paragraph3");
+description3.innerHTML =  "Escalada é um esporte que utiliza as técnicas e movimentos do montanhismo. Pode ser praticada tanto individualmente como em grupo e tem como objetivo exigir o máximo de força e concentração. ... A escalada de bloco consiste em subir uma rocha ou um muro de treino, onde se privilegia mais a força física de explosão."
+
+let description4 = document.getElementById("paragraph4");
+description4.innerHTML =  "Bicicleta (do francês bicyclette que deriva de bicycle união de bi, dois, com a palavra grega kyklos, rodas) é um veículo de duas rodas presas a um quadro, movido pelo esforço do próprio usuário (ciclista) através de pedais, sendo assim um velocípede de duas rodas."
+
+let pong = document.getElementById("ping");
+pong.href = "https://www.hobbytt.com.br/blog/regras-do-pingue-pongue-ou-ping-pong/regras-do-pingue-pongue";
+
+let fut = document.getElementById("futebol");
+fut.href = "https://ludopedio.org.br/produto/logicas-no-futebol-releituras/";
+
+let escalar1 = document.getElementById("escalar");
+escalar1.href = "https://mundoeducacao.uol.com.br/educacao-fisica/escalada.htm#:~:text=A%20escalada%20%C3%A9%20um%20esporte,m%C3%A1ximo%20de%20for%C3%A7a%20e%20concentra%C3%A7%C3%A3o.";
+
+let bike = document.getElementById("bike");
+bike.href = "https://www.metlife.com.br/blog/saude-e-bem-estar/10-beneficios-de-andar-de-bicicleta-para-a-sua-saude/#:~:text=Andar%20de%20bicicleta%20%C3%A9%20um,consequentemente%2C%20uma%20silhueta%20mais%20magra.&text=Como%20todo%20exerc%C3%ADcio%20f%C3%ADsico%2C%20pedalar%20exige%20for%C3%A7a%20dos%20m%C3%BAsculos.";
+
+
+let pingPong = document.getElementById("hobby-1");
+
+pingPong.src = "img/ping.jpg";
+
+let futebol = document.getElementById("hobby-2");
+
+futebol.src = "img/fut.jpg";
+
+let escalar = document.getElementById("hobby-3");
+
+escalar.src = "img/escalar.jpg";
+
+let bicicleta = document.getElementById("hobby-4");
+
+bicicleta.src = "img/bike.jpg";
+
+
+
+
