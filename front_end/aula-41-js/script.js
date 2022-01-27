78eddd4009351eb9d872ae3841e30b12ad7a1138
Paulo Alexandre <pas.cje.empreender@gmail.com>
2022-01-27 08:56:12 -0300

    Funçoes Recursivas com Matrizes

--- front_end/aula-41-js/script.js
+++ front_end/aula-41-js/script.js
@@ -1,12 +1,12 @@
-// Crie uma função recursiva que retorne o fatorial de um número inteiro.
+// 1. Crie uma função recursiva que retorne o fatorial de um número inteiro.
 
-const fatorial = (n) => (n === 1) ? 1 : (n * fatorial(n - 1));
+const fatorial = (n) => (n === 1) ? n : (n * fatorial(n - 1));
 
 console.log(fatorial(3));
 console.log(fatorial(5));
 console.log(fatorial(6));
 
-// Crie uma função recursiva que receba como parâmetro uma matriz e seja capaz de exibir no console todos os dados que a compõe. 
+// 2. Crie uma função recursiva que receba como parâmetro uma matriz e seja capaz de exibir no console todos os dados que a compõe. 
 
 const matriz = [
     [2, 3, 4],
@@ -14,21 +14,22 @@ const matriz = [
     [9, 8, 7],
 ];
 
-const matrizes = (n1) => {
-    if (n1.length === 0) {
-        return n1[0];
+const matrizes = (rows) => {
+    if (rows.length === 0) {
+        return rows[0];
     }
 
-    const matriz = (n2) => {
-        if (n2.length === 0) {
-            return n2[0];
+    const matriz = (columns) => {
+        if (columns.length === 0) {
+            return columns[0];
         }
-        console.log(n2.shift());
-        matriz(n2);
+        
+        console.log(columns.shift());
+        matriz(columns);
     }
 
-    matriz(n1.shift());
-    return matrizes(n1);
+    matriz(rows.shift());
+    return matrizes(rows);
 }
 
 matrizes(matriz);
